9923f261a38df4d7bbcd0b2f8ff80ebf7860c78a
Ben Kurtovic <ben.kurtovic@gmail.com>
2016-04-21 22:34:39 -0500

    Add runtime library.

---
+++ lib/kgrader/runtime.rb
@@ -0,0 +1,31 @@
+require 'timeout'
+
+module KGrader
+  module Runtime
+    MAX_COLS = 79
+
+    def testcase(options)
+      puts " running test: #{$0} ".center MAX_COLS, '='
+      begin
+        Timeout::timeout options[:alarm] { yield }
+      rescue Timeout::Error
+        comment "timeout"
+        grade 0
+      end
+      puts " done ".center MAX_COLS, '-'
+    end
+
+    def grade(score)
+      IO.new(3).write score
+      exit
+    end
+
+    def comment(text)
+      IO.new(4).write text + "\n"
+    end
+
+    def shake
+      ARGV[0]
+    end
+  end
+end
